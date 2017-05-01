205ea02f51d40abacbcbbf11c64879e518475965
abudaan <daniel@abumarkub.net>
2017-05-02 00:41:08 +0200

    first connected version: hover view1 triggers highlight in view2

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const alphabet = 'ABCDEFGHIJK';
 
 let view1;
 let view2;
+let highlightedPoint = null;
 
 fetchJSON('./assets/data/vega1.vg.json')
 .then((data) => {
@@ -31,7 +32,19 @@ fetchJSON('./assets/data/vega1.vg.json')
     view1.addSignalListener('tooltip', (name, data) => {
         if (R.isNil(data.category) === false) {
             const i = R.findIndex(c => c === R.toUpper(data.category))(alphabet);
-            console.log(data.category, i);
+            const newHighlightedPoint = {
+                category: i + 1,
+                amount: data.amount,
+            };
+            view2
+            .change('highlightedPoint',
+                vega.changeset()
+                .insert([newHighlightedPoint])
+                .remove(highlightedPoint),
+            )
+            .run();
+            highlightedPoint = newHighlightedPoint;
+            // console.log(data.category, i);
         }
     });
 });
