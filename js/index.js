ade4761fb2adc71fe3aabc71050482dc6a457553
abudaan <daniel@abumarkub.net>
2017-05-03 00:23:01 +0200

    wip

--- js/index.js
+++ js/index.js
@@ -3,72 +3,47 @@ import 'babel-polyfill';
 import R from 'ramda';
 import { fetchJSON } from './util/fetch_helpers';
 
-const vega = global.vega;
-const alphabet = 'ABCDEFGHIJK';
-
+const vega = global.vega; // coding like it's 1999
+let highlight;
 let view1;
 let view2;
-let highlightedPoint = null;
 
 fetchJSON('./assets/data/vega1.vg.json')
 .then((data) => {
     view1 = new vega.View(vega.parse(data))
     .renderer('canvas')
     .initialize('#view1')
-    .hover()
+    // .hover()
     .run();
 })
 .then(() => fetchJSON('./assets/data/vega2.vg.json'))
 .then((data) => {
     view2 = new vega.View(vega.parse(data))
-    .renderer('canvas')
+    .renderer('svg')
     .initialize('#view2')
+    // .hover()
     .run();
 })
 .then(() => {
+    /*
     view1.addSignalListener('tooltip', (name, data) => {
-        console.log('aaap', data);
-        if (R.isNil(data.category) === false) {
-            const i = R.findIndex(c => c === R.toUpper(data.category))(alphabet);
-            const newHighlightedPoint = {
-                category: i + 1,
-                amount: data.amount,
-            };
-            console.log(newHighlightedPoint);
-            view2
-            .change('highlightedPoint',
-                vega.changeset()
-                .insert([newHighlightedPoint])
-                .remove(highlightedPoint),
-            )
-            .run();
-            highlightedPoint = newHighlightedPoint;
-            // console.log(view1.getState().signals.tooltip);
-            view2.signal('tooltip', newHighlightedPoint).run();
-        } else {
-            view2
-            .change('highlightedPoint',
-                vega.changeset()
-                .remove(highlightedPoint),
-            )
-            .run();
-            view2.signal('tooltip', highlightedPoint).run();
-        }
+        view2.signal('tooltip', data).run();
+        // view2.change('highlight',
+        //     vega.changeset()
+        //     .insert([data])
+        //     .remove(highlight),
+        // ).run();
+        // highlight = data;
     });
-    // view1.signal('tooltip', []).run();
-    // console.log(view1);
-    view2.signal('tooltip', view1.data('table')[2]).run();
-    // const p = view2.signal('height', 14).run();
-    // console.log(p);
-    // const state = view1.getState();
-    // console.log(state);
-    // view1.setState({
-    //     ...state,
-    //     signals: {
-    //         ...state.signals,
-    //         tooltip: [{ category: 'B', amount: 55, _id: 3 }],
-    //     },
-    // })
-    // .run();
+    */
+    view2.addSignalListener('tooltip', (name, data) => {
+        view1.signal('tooltip', data).run();
+        console.log(view1.getState());
+        // view1.change('highlight',
+        //     vega.changeset()
+        //     .insert([data])
+        //     .remove(highlight),
+        // ).run();
+        // highlight = data;
+    });
 });
-
