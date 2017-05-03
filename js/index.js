eacd60cfafa8e869976490f665f7ce8b437f077f
abudaan <daniel@abumarkub.net>
2017-05-04 00:27:16 +0200

    2-way interactivity

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const vega = global.vega; // coding like it's 1999
 let view1;
 let view2;
 const now = Date.now();
+let dataState = {};
 
 const signalListenerView1 = (name, data) => {
     view1.signal('tooltip', data).run();
@@ -30,30 +31,62 @@ fetchJSON(`./assets/data/vega1.vg.json?${now}`)
     .initialize('#view2');
 })
 .then(() => fetchJSON(`./assets/data/data.json?${now}`))
-.then((data) => {
+.then((dataset) => {
+    dataState = R.clone(dataset);
     view1.change('table',
         vega.changeset()
-        .insert(data),
+        .insert(dataset),
     ).run();
     view2.change('table',
         vega.changeset()
-        .insert(data),
+        .insert(dataset),
     ).run();
 
     view1.addSignalListener('tooltip', signalListenerView2);
     view2.addSignalListener('tooltip', signalListenerView1);
 
-    view1.addSignalListener('changeAmount', (name, d) => {
-        view1.change('table',
-            vega.changeset()
-            .insert(d)
-            .remove(datum => datum.category === d.category),
-        ).run();
+    view1.addSignalListener('changeAmount', (name, data) => {
+        const index = R.findIndex(R.propEq('category', data.category))(dataState);
+        if (index !== -1) {
+            const catLens = R.lensIndex(index);
+            dataState = R.set(catLens, data, dataState);
+            // view2.remove('table', d => d.category === data.category).run();
+            // view2.insert('table', data).run();
 
-        view2.change('table',
-            vega.changeset()
-            .insert(d)
-            .remove(datum => datum.category === d.category),
-        ).run();
+            setTimeout(() => {
+                // view1.remove('table', d => d.category === data.category).run();
+                // view1.insert('table', data).run();
+                view1.remove('table', () => true).run();
+                view1.insert('table', dataState).run();
+            }, 0);
+
+            // view2.change('table', vega.changeset().remove(dataset).insert(clone)).run();
+            view2.remove('table', () => true).run();
+            view2.insert('table', dataState).run();
+        }
+    });
+
+    view2.addSignalListener('changeAmount', (name, data) => {
+        const index = R.findIndex(R.propEq('category', data.category))(dataState);
+        if (index !== -1) {
+            const catLens = R.lensIndex(index);
+            dataState = R.set(catLens, data, dataState);
+
+            setTimeout(() => {
+                view2.remove('table', () => true).run();
+                view2.insert('table', dataState).run();
+            }, 0);
+
+            view1.remove('table', () => true).run();
+            view1.insert('table', dataState).run();
+        }
+    });
+
+    view2.addSignalListener('selectedCategory', (name, category) => {
+        view1.signal('selectedCategory', category).run();
+    });
+
+    view1.addSignalListener('selectedCategory', (name, category) => {
+        view2.signal('selectedCategory', category).run();
     });
 });
