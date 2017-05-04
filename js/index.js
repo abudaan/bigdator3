40fd3dfeb36b0e5db7cf68846101e4827c309d79
abudaan <daniel@abumarkub.net>
2017-05-04 19:32:59 +0200

    updated 2nd view

--- js/index.js
+++ js/index.js
@@ -39,14 +39,20 @@ fetchJSON(`./assets/data/vega1.vg.json?${now}`)
         view2.addSignalListener('tooltip', (name, data) => {
             view1.signal('tooltip', data).run();
         });
+
         view1.addSignalListener('dataUpdate', (name, data) => {
             view2.remove(data.name, () => true).run();
             view2.insert(data.name, data.values).run();
         });
-        view2.addSignalListener('selectedCategory', (name, category) => {
-            view1.signal('selectedCategory', category).run();
+        view2.addSignalListener('dataUpdate', (name, data) => {
+            view1.remove(data.name, () => true).run();
+            view1.insert(data.name, data.values).run();
         });
+
         view1.addSignalListener('selectedCategory', (name, category) => {
             view2.signal('selectedCategory', category).run();
+        });
+        view2.addSignalListener('selectedCategory', (name, category) => {
+            view1.signal('selectedCategory', category).run();
         });
     });
